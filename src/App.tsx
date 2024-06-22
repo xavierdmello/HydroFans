9b8b40734cf5760425fe3003b815088d35dd1a88
Xavier DMello <xaviermdmello@gmail.com>
2024-06-22 13:17:30 -0700

    yoo

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import logo from "/logo.png";
 function App() {
   return (
     <div className="min-h-screen bg-[#00afef] flex items-center justify-center py-4">
-      <div className="w-4/5 max-w-[800px] mx-auto flex flex-col items-center bg-white rounded-lg shadow-lg h-full min-h-[calc(100vh-2rem)]">
+      <div className="w-4/5 max-w-[800px] mx-auto flex flex-col items-center bg-white rounded-2xl shadow-lg h-full min-h-[calc(100vh-2rem)]">
         <img src={logo} className="w-64 mt-8" alt="Hydrofans" />
       </div>
     </div>
