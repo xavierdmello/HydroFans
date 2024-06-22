d2e8ed2de4cf60697d9f409ece7a83ad5af5a408
Xavier DMello <xaviermdmello@gmail.com>
2024-06-22 13:17:05 -0700

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import logo from "/logo.png";
 
 function App() {
   return (
-    <div className="w-4/5 max-w-[1200px] mx-auto flex flex-col items-center bg-white h-full min-h-screen">
-      <img src={logo} className="w-64" alt="Hydrofans" />
+    <div className="min-h-screen bg-[#00afef] flex items-center justify-center py-4">
+      <div className="w-4/5 max-w-[800px] mx-auto flex flex-col items-center bg-white rounded-lg shadow-lg h-full min-h-[calc(100vh-2rem)]">
+        <img src={logo} className="w-64 mt-8" alt="Hydrofans" />
+      </div>
     </div>
   );
 }
